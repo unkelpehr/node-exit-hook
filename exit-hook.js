397f103cad189ea5be02116ac173bd0562ab35dc
Pehr Boman <unkelpehr@gmail.com>
2015-12-13 00:12:23 +0100

    Initial commit

---
+++ exit-hook.js
@@ -0,0 +1,88 @@
+// Licenced under MIT - exit-hook - ©2015 Pehr Boman <github.com/unkelpehr>
+'use strict';
+
+var funcs = [],
+	win32 = process.platform === 'win32',
+	bound = {};
+
+// Handler for all exit events
+function handleExit (canCancel, signal, code) {
+	var i = 0,
+		func,
+		allow = true;
+
+	while ((func = funcs[i++])) {
+		allow = func(canCancel, signal, code) === false ? false : allow;
+	}
+
+	if (canCancel && allow) {
+		process.exit(code);
+	}
+};
+
+//  Adds a new function to execute when the application is exiting.
+function exitHook (func) {
+	if (typeof func === 'function') {
+		funcs.push(func)
+	}
+
+	return exitHook;
+};
+
+// Binds a new event to treat as a shutdown signal.
+exitHook.bind = function (signal, canCancel) {
+	if (signal && typeof signal === 'string' && !bound[signal]) {
+		bound[signal] = function (code) {
+			handleExit(canCancel, signal, code);
+		};
+
+		process.on(signal, bound[signal]);
+	}
+
+	return exitHook;
+};
+
+// Unbinds a previously bound shutdown signal.
+exitHook.unbind = function (signal) {
+	if (bound[signal]) {
+		process.removeListener(signal, bound[signal]);
+		delete bound[signal];
+	} else if (signal === 'everything') {
+		Object.keys(bound).forEach(function (signal) {
+			process.removeListener(signal, bound[signal]);
+			delete bound[signal];
+		});
+	}
+
+	return exitHook;
+};
+
+// Returns an array with all the events currently listened to.
+exitHook.list = function () {
+	return Object.keys(bound);
+};
+
+// SIGINT from the terminal is supported on all platforms, and can usually be generated with
+// CTRL+C (though this may be configurable). It is not generated when terminal raw mode is enabled.
+exitHook.bind('SIGINT', true);
+
+// SIGTERM is not supported on Windows but it can be listened for.
+exitHook.bind('SIGTERM', true);
+
+// SIGBREAK is delivered on Windows when CTRL+BREAK is pressed.
+// There is no way to send or generate it.
+exitHook.bind('SIGBREAK', true);
+
+// SIGHUP is generated on Windows when the console window is closed and will unconditionally terminate the application about 5 seconds later.
+// SIGHUP is generated on non-Windows platforms under various similar conditions but once listened to its default behaviour is removed.
+// 
+// On non-Windows platforms we will effectively bring back its default behaviour to terminate Node.js (albeit cancellable).
+exitHook.bind('SIGHUP', !win32);
+
+// Emitted when Node.js empties its event loop and has nothing else to schedule.
+exitHook.bind('beforeExit', false);
+
+// Emitted right before the process is about to exit and once all 'exit' listeners have finished the process will exit.
+exitHook.bind('exit', false);
+
+module.exports = exitHook;
---
+++ test/tests/basics.js
@@ -0,0 +1,177 @@
+'use strict';
+
+var expect = require('chai').expect,
+	exitHook = require('../../exit-hook.js');
+
+function requireExitHook () {
+	var keys, key, i;
+
+	if (exitHook) {
+		exitHook.unbind('everything');
+
+		i = 0;
+		keys = Object.keys(require.cache);
+
+		while ((key = keys[i++])) {
+			if (key.substr(-12) === 'exit-hook.js') {
+				delete require.cache[key];
+				break;
+			}
+		}
+	}
+	
+	return require('../../exit-hook.js');
+}
+
+describe('API', function () {
+	var exitHook = requireExitHook();
+
+	it('exit-hook is a function that take one argument and returns exit-hook', function () {
+		expect(exitHook).to.be.a('function');
+		expect(exitHook.length).to.equal(1);
+		expect(exitHook()).to.equal(exitHook);
+	});
+
+	it("exit-hook has only three properties: 'bind', 'unbind', and 'list", function () {
+		expect(Object.keys(exitHook).sort()).to.be.deep.equal(['bind', 'list', 'unbind']);
+	});
+
+	it('exit-hook.bind is a function that take two arguments and returns exit-hook', function () {
+		expect(exitHook.bind).to.be.a('function');
+		expect(exitHook.bind.length).to.equal(2);
+		expect(exitHook.bind()).to.equal(exitHook);
+	});
+
+	it('exit-hook.unbind is a function that take one argument and returns exit-hook', function () {
+		expect(exitHook.unbind).to.be.a('function');
+		expect(exitHook.unbind.length).to.equal(1);
+		expect(exitHook.unbind()).to.equal(exitHook);
+	});
+
+	it('exit-hook.list is a function that take zero arguments', function () {
+		expect(exitHook.list).to.be.a('function');
+		expect(exitHook.list.length).to.equal(0);
+	});
+});
+
+describe('Basic functionality', function () {
+	var exitHook;
+
+	beforeEach('Resetting exit-hook', function () {
+		exitHook = requireExitHook();
+	});
+
+	it('exit-hook.list returns an array of bound events', function () {
+		var list1 = exitHook.list(),
+			list2;
+
+		expect(exitHook.list()).to.be.an('array');
+
+		exitHook.bind('CUSTOM1');
+
+		list2 = exitHook.list();
+
+		expect(list2.length).to.equal(list1.length + 1);
+		expect(list2[list2.length - 1]).to.equal('CUSTOM1');
+	});
+
+	it('exit-hook.unbind(\'everything\' removes all listeners', function () {
+		exitHook.unbind('everything');
+		expect(exitHook.list().length).to.equal(0);
+	});
+
+	it('exit-hook.unbind(\'CUSTOM1\' removes the CUSTOM1-listener', function () {
+		var list1 = exitHook.list(),
+			list2;
+
+		exitHook.bind('CUSTOM1').unbind('CUSTOM1');
+
+		list2 = exitHook.list();
+
+		expect(list1).to.deep.equal(list1);
+	});
+
+	it('exit-hook.bind adds a new event to listen for', function (done) {
+		var signal = 'CUSTOM1',
+			code = 446;
+
+		exitHook.bind(signal, true);
+
+		expect(exitHook.list().slice(-1)[0]).to.equal(signal);
+
+		exitHook(function (canCancel, _signal, _code) {
+			expect(canCancel).to.equal(true);
+			expect(_signal).to.equal(signal);
+			expect(_code).to.equal(code);
+
+			done();
+
+			exitHook.unbind('everything');
+			
+			return false;
+		});
+
+		process.emit(signal, code);
+	});
+});
+
+describe('Extended functionality', function () {
+	var exitHook;
+	
+	beforeEach('Resetting exit-hook', function () {
+		exitHook = requireExitHook();
+	});
+
+	it('Don\'t allow multiple binds on the same event', function () {
+		var list1,
+			list2;
+
+		exitHook.bind('CUSTOM1');
+
+		list1 = exitHook.list();
+
+		exitHook.bind('CUSTOM1').bind('CUSTOM1').bind('CUSTOM1');
+
+		list2 = exitHook.list();
+
+		expect(list1.length).to.deep.equal(list2.length);
+	});
+
+	it('Returning false doesn\'t let the process terminate', function (done) {
+		exitHook(function () {
+			setTimeout(done);
+
+			return false;
+		}).bind('CUSTOM1');
+
+		process.emit('CUSTOM1');
+	});
+	
+	it('Multiple exithooks can be set', function (done) {
+		var calls = 3;
+
+		function next () {
+			if (!--calls) {
+				done();
+
+				exitHook.unbind('everything');
+			}
+
+			return false;
+		}
+
+		exitHook.bind('CUSTOM2');
+
+		exitHook(next);
+		exitHook(next);
+		exitHook(next);
+
+		process.emit('CUSTOM2');
+	});
+
+	it('Last test. A non-cancellable signal (I guess mocha doesn\'t let the event-loop run out)', function () {
+		exitHook(function (canCancel, signal, code) {			
+			expect(canCancel).to.equal(false);
+		});
+	});
+});
